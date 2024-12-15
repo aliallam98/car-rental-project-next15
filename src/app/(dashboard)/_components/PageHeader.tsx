69e0fb4f49c8cc307ccf98cb770c1c72a5e8625c
aliallam98 <allam98.ali@gmail.com>
2024-12-15 19:28:11 +0200

    #0.0.1 added reusable 2 components SectionWithContainer and PageHeader

---
+++ src/app/(dashboard)/_components/PageHeader.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import {  LucideIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+interface IProps {
+  headerTitle: string;
+  buttonTitle: string;
+  linkHref: string;
+  icon: LucideIcon;
+}
+
+export function PageHeader({ headerTitle,buttonTitle, linkHref, icon: Icon }: IProps) {
+  return (
+    <div className="flex items-center justify-between p-4 mb-6 border-b bg-background shadow-sm">
+      <h1 className="text-2xl font-bold">{headerTitle}</h1>
+      <Button variant="ghost" size="sm" asChild>
+        <Link href={linkHref} className="flex items-center">
+          <Icon className="mr-2 h-4 w-4" />
+          {buttonTitle}
+        </Link>
+      </Button>
+    </div>
+  );
+}
---
+++ src/components/SectionWithContainer.tsx
@@ -0,0 +1,9 @@
+const SectionWithContainer = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <section className="py-10 md:py-20">
+      <div className="container">{children}</div>
+    </section>
+  );
+};
+
+export default SectionWithContainer;
